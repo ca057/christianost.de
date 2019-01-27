0f00cba70b4c32a2933008a95c9f0d081eb60c7a
Christian Ost <christian@christianost.de>
2019-01-27 13:42:37 +0100

    first tests

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('hello');
+require('dotenv').config();
+
+const { makeGetRequest } = require('./lib/request');
+
+const TOGGL_API_TOKEN = process.env.toggl_api_token;
+
+makeGetRequest({
+  host: 'toggl.com',
+  path: '/reports/api/v2/weekly?user_agent=chrstnst&workspace_id=3154506',
+  auth: `${TOGGL_API_TOKEN}:api_token`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then(console.log, console.error);
