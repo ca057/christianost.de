c3c840939127d43aab3ea0a393ac9fdb333e86df
Christian Ost <christian@christianost.de>
2019-02-11 19:20:54 +0100

    feat: basic line drawing

--- src/modules/lines.js
+++ src/modules/lines.js
@@ -26,35 +26,24 @@ function drawLines(element, lineData) {
 
   const unit = two.height / lineData.lines.length;
   const center = { x: two.width / 2, y: two.height / 2 };
-  console.log({ two, unit });
 
   let lastPoint = center;
 
-  lineData.lines.slice(0, 2).forEach(({ value }) => {
+  lineData.lines.forEach(({ value }) => {
     const line = two.makeLine(0, 0, unit, 0);
-    line.linewidth = 5;
-    line.translation.set(center.x, center.y);
+
     const valueAsRadians = toRadians(360 * value);
+    const nextPoint = {
+      x: lastPoint.x + unit * Math.cos(valueAsRadians),
+      y: lastPoint.y + unit * Math.sin(valueAsRadians),
+    };
+
+    line.translation.set(lastPoint.x, lastPoint.y);
+    line.linewidth = 5;
     line.rotation = valueAsRadians;
 
-    // x = cx + r * cos(a)
-    // y = cy + r * sin(a)
-    console.log({
-      nextX: center.x + unit * Math.cos(valueAsRadians),
-      nextY: center.y + unit * Math.sin(valueAsRadians),
-      valueAsRadians,
-      valueAsDegrees: 360 * value,
-    });
-    two.makeCircle(
-      center.x + unit * Math.cos(valueAsRadians),
-      center.y + unit * Math.sin(valueAsRadians),
-      5,
-    );
-
-    console.log(line, 360 * value);
+    lastPoint = nextPoint;
   });
-
-  // two.makeCircle(center.x, center.y, 5);
 
   two.update();
 }
