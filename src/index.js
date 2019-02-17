d94a9d9e134b76fa21d8a94fd8e11aac985368d3
Christian Ost <christian@christianost.de>
2019-02-17 22:14:46 +0100

    fix: insert line drawing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createFontsLoader, { addFontLoadedClass } from './modules/fonts';
+import drawLines from './modules/lines';
 
 const fontsLoader = createFontsLoader();
 
 const handleDocumentReady = () => {
   fontsLoader.then(addFontLoadedClass);
+  drawLines(document.getElementById('lines-space'));
 };
 
 if (document.readyState === 'loading') {
