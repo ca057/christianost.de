16409bac3dc767a4ba05f12d0b3c197b84a54124
Christian Ost <christian@christianost.de>
2019-03-04 07:50:24 +0100

    refactor: remove unused code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import createFontsLoader, { addFontLoadedClass } from './modules/fonts';
-import drawLines from './modules/lines';
 
 const fontsLoader = createFontsLoader();
 
 const handleDocumentReady = () => {
   fontsLoader.then(addFontLoadedClass);
-  drawLines(document.getElementById('lines-space'));
 };
 
 if (document.readyState === 'loading') {
