fb079ab7faf473f8ca0b447fee1437feb18fde40
Christian Ost <christian@christianost.de>
2019-08-05 22:58:35 +0200

    fix: cleanup after rebasing

--- src/modules/fonts.js
+++
@@ -1,9 +0,0 @@
-import FontFaceObserver from 'fontfaceobserver';
-
-export const addFontLoadedClass = () => {
-  document.body.classList.add('fonts-loaded');
-};
-
-const interFontObserver = new FontFaceObserver('Inter Regular');
-
-export default () => interFontObserver.load();
