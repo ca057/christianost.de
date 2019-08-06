bf2c41582a27b50c8d57dca1ec160a33c9c9a562
Christian Ost <christian@christianost.de>
2019-08-06 07:44:31 +0200

    fix: simplify build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 set -ev
 
-deploy_assets_to_server () {
-  assets_path=$1
-
-  echo "DEPLOY: Start deploying assets to remote."
-
-  export SSHPASS=$deploy_password
-  sshpass -e scp -o stricthostkeychecking=no -r $assets_path $deploy_user@$deploy_host:$deploy_path
-  export SSHPASS=
-}
-
 echo "Starting deployment."
 echo ""
 
-if [ ! -d "$web_build_dir" ]; then
+if [ ! -d "dist" ]; then
   echo "ERROR: The build directory does not exist."
   exit 1
 fi
 
-deploy_assets_to_server $web_build_dir
+echo "DEPLOY: Start deploying assets to remote."
+
+export SSHPASS=$deploy_password
+
+sshpass -e scp -o stricthostkeychecking=no -r dist/. $deploy_user@$deploy_host:$deploy_path
+
+export SSHPASS=
