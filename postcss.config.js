0b6bb42d026c0cd03f81bf819bfde376a267daba
Christian Ost <hallo@christianost.de>
2020-03-29 00:52:08 +0100

    feat: better asset handling, simpler configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     "postcss-preset-env": {
-      browsers: "defaults"
+      browsers: "last 2 versions"
     },
     autoprefixer: {}
   }
--- webpack.dev.js
+++ webpack.dev.js
@@ -15,7 +15,7 @@ module.exports = merge(common, {
 
   devServer: {
     port: process.env.PORT || 3000,
-    contentBase: path.join(process.cwd(), "./dist"),
+    contentBase: path.join(process.cwd(), "./public"),
     watchContentBase: true,
     quiet: false,
     open: true,
@@ -27,8 +27,8 @@ module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [
-        "dist/**/*.js",
-        "dist/**/*.css",
+        "public/**/*.js",
+        "public/**/*.css",
         "content/webpack.json"
       ]
     }),
