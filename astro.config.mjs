314d51f0388974aa26e5d297ac4d9e712c53bb39
Christian Ost <hallo@christianost.de>
2022-09-29 23:37:18 +0200

    fix: remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [image()],
   output: "server",
-  adapter: netlify()
 });
