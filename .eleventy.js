7343afb9ce891900b0231c05d5fab8fb54cc45f0
Christian Ost <hallo@christianost.de>
2024-10-03 23:18:30 +0200

    feat: basic config with njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,54 @@
-export default function(eleventyConfig) {
+import { bundle, browserslistToTargets, transform } from "lightningcss";
+import browserslist from "browserslist";
+
+const environment =
+  process.env.ELEVENTY_RUN_MODE === "serve" || process.env.ELEVENTY_RUN_MODE === "watch" ? "development" : "production";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addTemplateFormats("css");
+
+  eleventyConfig.addExtension("css", {
+    outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      let targets = browserslistToTargets(browserslist("> 0.2% and not dead")); // TOOD:
+
+      return async () => {
+        let { code, warnings } = bundle({
+          filename: inputPath,
+          minify: environment === "production",
+          sourceMap: true,
+          targets,
+          drafts: { nesting: true },
+        });
+
+        if (warnings.length > 0) {
+          console.warn(warnings);
+        }
+
+        return code;
+      };
+    },
+  });
+
+  eleventyConfig.addFilter("cssmin", function (input) {
+    const { code, warnings } = transform({
+      // TODO:
+      code: Buffer.from(input),
+      minify: environment === "production",
+      sourceMap: true,
+    });
+
+    if (warnings.length > 0) {
+      console.warn(warnings);
+    }
+
+    return code.toString("utf-8");
+  });
+
   return {
     dir: {
       input: "src",
-      output: "dist"
-    }
-  }
+      output: "dist",
+    },
+  };
 }
