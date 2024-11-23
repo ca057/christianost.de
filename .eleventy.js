1ed7ce32a9b5861b34c38402cd2d2c6594a5daf9
Christian Ost <hallo@christianost.de>
2024-11-23 13:26:49 +0100

    feat: some progress on the coffee

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 // import { bundle, browserslistToTargets, transform } from "lightningcss";
 // import browserslist from "browserslist";
 import pluginWebc from "@11ty/eleventy-plugin-webc";
-import { Temporal } from "temporal-polyfill";
+import { getBlurHashAverageColor } from "fast-blurhash";
 
 const environment =
   process.env.ELEVENTY_RUN_MODE === "serve" || process.env.ELEVENTY_RUN_MODE === "watch" ? "development" : "production";
@@ -12,60 +12,11 @@ export default function (eleventyConfig) {
   });
 
   eleventyConfig.addBundle("css");
-  // eleventyConfig.addTemplateFormats("css");
-  // eleventyConfig.addExtension("css", {
-  //   outputFileExtension: "css",
-  //   compile: async function (inputContent, inputPath) {
-  //     let targets = browserslistToTargets(browserslist("> 0.2% and not dead")); // TOOD:
-
-  //     return async () => {
-  //       let { code, warnings } = bundle({
-  //         filename: inputPath,
-  //         minify: environment === "production",
-  //         sourceMap: true,
-  //         targets,
-  //         drafts: { nesting: true },
-  //       });
-
-  //       if (warnings.length > 0) {
-  //         console.warn(warnings);
-  //       }
-
-  //       return code;
-  //     };
-  //   },
-  // });
-
-  // eleventyConfig.addFilter("cssmin", function (input) {
-  //   const { code, warnings } = transform({
-  //     // TODO:
-  //     code: Buffer.from(input),
-  //     minify: environment === "production",
-  //     sourceMap: true,
-  //   });
-
-  //   if (warnings.length > 0) {
-  //     console.warn(warnings);
-  //   }
-
-  //   return code.toString("utf-8");
-  // });
-  //
-  //
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy("src/favicon");
+  eleventyConfig.addPassthroughCopy("src/**/images/*");
 
-  eleventyConfig.addGlobalData("coffees", async () => {
-    // TODO: load this from externally
-
-    //    if (environment === "development") {
-    const now = Temporal.PlainDate.from(Temporal.Now.plainDateISO());
-
-    return Array.from({ length: 3451 }).map((_, i) => ({
-      day: now.subtract({ days: i }),
-    }));
-    //    }
-  });
+  eleventyConfig.addFilter("blurhashColorRgb", (blurhash) => `rgb(${getBlurHashAverageColor(blurhash).join(",")})`);
 
   return {
     dir: {
