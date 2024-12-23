636fda8d315e29d6475a2d2b86fe65ae43d7fc1b
Christian Ost <hallo@christianost.de>
2024-12-23 16:57:59 +0100

    fix: a lot of things fixed and stuff added, no clue

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,33 @@
 // TODO: remove it
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 import pluginFavicon from "eleventy-plugin-gen-favicons"
-import { getBlurHashAverageColor } from "fast-blurhash";
+import { decodeBlurHash, getBlurHashAverageColor } from "fast-blurhash";
+import sharp from "sharp";
 import { Temporal } from "temporal-polyfill";
+import { readFile } from "node:fs/promises"
 
 const environment =
   process.env.ELEVENTY_RUN_MODE === "serve" || process.env.ELEVENTY_RUN_MODE === "watch" ? "development" : "production";
 
-export default function (eleventyConfig) {
+/**
+*
+* @param {string} blurHash
+*/
+async function blurhashToFavicon(blurHash, outputPath) {
+  const size = 32
+  const pixels = decodeBlurHash(blurHash, size, size)
+  const pixelBuffer = Buffer.from(pixels)
+
+  await sharp(pixelBuffer, {
+    raw: {
+      width: size,
+      height: size,
+      channels: 4
+    }
+  }).toFormat("png").toFile("./faaaaa.png")
+}
+
+export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebc, {
     components: ["src/**/_components/**/*.webc"],
   });
@@ -24,6 +44,10 @@ export default function (eleventyConfig) {
   eleventyConfig.addFilter("formatDate", (date, locale = "en-UK") => {
     return Temporal.PlainDate.from(date).toLocaleString(locale, { calendar: 'gregory', dateStyle: "long" })
   })
+
+  const coffees = await readFile("src/_data/coffees.json", { encoding: "utf-8" }).then(f => JSON.parse(f))
+  // TODO: use zod or similar
+  await blurhashToFavicon(coffees.images[0].images.blurhash)
 
   return {
     dir: {
