9343bede609cefcb9aadaed14ec3d0a77a83fd0e
Christian Ost <hallo@christianost.de>
2025-01-05 22:17:46 +0100

    fix: cleanup

--- src/coffees/overlay.js
+++
@@ -1,9 +0,0 @@
-function addImgListeners() {
-  console.log(document.readyState);
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", addImgListeners);
-} else {
-  addImgListeners();
-}
